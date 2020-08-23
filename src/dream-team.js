f67cb85e81731edea0731b7cba24f33ed49f85ef
Andrei Miashkou <meshkow.andrei@gmail.com>
2020-08-23 21:53:03 +0300

    feat: add solution dream-team, hanoi-tower and what-season

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,12 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+	if (!Array.isArray(members)) return false;
+	const arr = [];
+	members.forEach(val => {
+		if (typeof (val) === 'string') {
+			arr.push(val.trim()[0].toUpperCase())
+		}
+	})
+	return arr.sort().join('');
 };
--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,14 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+	console.log(Object.prototype.toString.call(date));
+	console.log(date);
+	const month = date.getMonth();
+	if (Object.prototype.toString.call(date) === '[object Date]') {
+		return month < 2 ? 'winter' : month < 5 ? 'spring' : month < 8 ? 'summer' : month < 11 ? 'autumn' : 'winter';
+	} else if (date === undefined) {
+		return 'Unable to determine the time of year!';
+	} else {
+		throw new Error();
+	}
 };
