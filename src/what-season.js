16e2b30810250067032c8d7f066f8efb1d6b9341
Andrei Miashkou <meshkow.andrei@gmail.com>
2020-08-25 12:22:14 +0300

    feat: add solution simple-chain

--- src/what-season.js
+++ src/what-season.js
@@ -1,8 +1,6 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function getSeason(date) {
-	console.log(Object.prototype.toString.call(date));
-	console.log(date);
 	const month = date.getMonth();
 	if (Object.prototype.toString.call(date) === '[object Date]') {
 		return month < 2 ? 'winter' : month < 5 ? 'spring' : month < 8 ? 'summer' : month < 11 ? 'autumn' : 'winter';
