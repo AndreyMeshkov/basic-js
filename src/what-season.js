e278d8dde5cb900fe6bda801fbe0754ed2fd2ef7
Andrei Miashkou <meshkow.andrei@gmail.com>
2020-08-26 13:16:50 +0300

    fix: fix error in what-season

--- src/what-season.js
+++ src/what-season.js
@@ -1,11 +1,12 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function getSeason(date) {
+	if (date === undefined) {
+		return 'Unable to determine the time of year!';
+	}
 	const month = date.getMonth();
 	if (Object.prototype.toString.call(date) === '[object Date]') {
 		return month < 2 ? 'winter' : month < 5 ? 'spring' : month < 8 ? 'summer' : month < 11 ? 'autumn' : 'winter';
-	} else if (date === undefined) {
-		return 'Unable to determine the time of year!';
 	} else {
 		throw new Error();
 	}
